03c4eb5fccd5e08b1d1003afa4698c603e324554
Rishi Gandikota <rishi.gandik@gmail.com>
2024-01-23 15:05:34 -0600

    Properly hosted Weather App

--- Week_4/weather-app/src/components/App.js
+++ Week_4/weather-app/src/components/App.js
@@ -6,7 +6,6 @@ import SideContainer from './SideContainer';
 
 // Assign valid OpenWeather API Key to apiKey to run app
 const apiKey = process.env.REACT_APP_API_KEY;
-console.log(apiKey)
 
 function App() {
   /*
